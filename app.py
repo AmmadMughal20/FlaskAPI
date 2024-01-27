db27e532807d03fcba0f84ee8109d50004d788ea
Muhammad Ammad <ammadmughal567@gmail.com>
2024-01-27 12:10:45 +0500

    Added a sample page

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return 'HELLO WORLDS'
+
+@app.route('/page')
+def page():
+    return render_template('Page.html')
