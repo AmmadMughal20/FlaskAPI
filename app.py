f7b99a0df1589ba676127359b2694236592df2f0
Muhammad Ammad <ammadmughal567@gmail.com>
2024-01-27 16:35:18 +0500

    Created users API

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, jsonify, request
+from db import db_connection
+import pyodbc
 
 
 app = Flask(__name__)
@@ -20,3 +22,36 @@ def api():
         }
 
         return jsonify(dictionary)
+    
+    
+@app.route('/users', methods=['GET', 'POST'])
+def users():
+    cnxn = db_connection()
+    try:
+        crsr = cnxn.cursor()
+        crsr.execute("SELECT * FROM users")
+        
+        columns = [column[0] for column in crsr.description]
+        rows = [dict(zip(columns, row)) for row in crsr.fetchall()]
+
+        if len(rows) > 0:
+            response_data = {
+                "status": 200,
+                "data": rows,
+                "message": "Data received successfully!"
+            }
+        else:
+            response_data = {
+                "status": 404,
+                "data": [],
+                "message": "No users found!"
+            }
+
+        return jsonify(response_data)
+    except pyodbc.Error as e:
+        print(e)
+        return jsonify({"status": 500, "message": "Internal Server Error"})
+    finally:
+        crsr.close()
+        cnxn.close()
+    
